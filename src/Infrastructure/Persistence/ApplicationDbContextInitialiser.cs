ace72ef98b2e3221c70da8f154af0068e8c85c93
Anthony Famorcan <anthony25famorcan@gmail.com>
2023-10-13 12:50:33 +0800

    Update migration files

--- src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
+++ src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -81,10 +81,10 @@ public class ApplicationDbContextInitialiser
                 Title = "Todo List",
                 Items =
                 {
-                    new TodoItem { Title = "Make a todo list 📃" },
-                    new TodoItem { Title = "Check off the first item ✅" },
-                    new TodoItem { Title = "Realise you've already done two things on the list! 🤯"},
-                    new TodoItem { Title = "Reward yourself with a nice, long nap 🏆" },
+                    new TodoItem { Title = "Make a todo listzz 📃" },
+                    new TodoItem { Title = "Check off the first itemzz ✅" },
+                    new TodoItem { Title = "Realise you've already done two things on the listzz! 🤯"},
+                    new TodoItem { Title = "Reward yourself with a nice, long napzz 🏆" },
                 }
             });
 
